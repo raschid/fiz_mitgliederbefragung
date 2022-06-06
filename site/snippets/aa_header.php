66de36ca4005406c188e024959430e0a60f38076
raschid <ralf_schmitt@yahoo.de>
2022-06-06 18:57:09 +0200

    app.css / header / footer
    - app.css eingefügt - noch leer
    - footer- und header-snippets  eingefügt
    - icons und bootstrap eingebunden
    - default-template angepasst.
    
    Titelzeile mit vfg- und fiz-logo muss noch angepasst werden!

---
+++ site/snippets/aa_header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<title>VFG Kiel e.V.: <?= $page->title(); ?></title>
+<?php 
+	print css('/assets/bootstrap-5.2.0/css/bootstrap.css')."\n";
+	print css('/assets/bootstrap-icons-1.8.3/bootstrap-icons.css')."\n";
+	print css('/assets/css/app.css'); 
+	print "\n".js('assets/bootstrap-5.2.0/js/bootstrap.bundle.min.js');
+?>
+</head>
+<body>
+<style type="text/css">
+	#mainContainer 
+	{
+		/*width: 100%px;*/
+		/*max-width:1320px;*/
+		margin:auto;
+	}
+</style>
+<div class="d-flex align-items-center bg-white ps-1 pe-1 py-1" style="border-bottom:2px solid #777; margin-bottom:1.5rem;">
+	<img src="assets/images/logo_vfg_57x50.png">
+	<div class="d-flex ms-auto">
+		<img src="assets/images/logo_fiz_138x50.png">
+	</div>
+</div>
+<div id="mainContainer" class="container-fluid">
+<div class="row">
+	<div class="col-xs-12 offset-0 col-md-10 offset-1">
+		<h1>Hallelujah!</h1>
+		
+	</div>
+</div>
+
+
