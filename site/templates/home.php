e14cd9ed4352833a9d9106e3bf1b0711f8ca184e
raschid <ralf_schmitt@yahoo.de>
2022-06-11 15:08:22 +0200

    Template home erstellt
    ...

---
+++ site/templates/home.php
@@ -0,0 +1,20 @@
+<?php echo snippet('aa_header'); ?>
+
+<h1><?php echo $page->title(); ?></h1>
+
+<p>
+	Willkommen zur ersten Mitgliederbefragung des VFG Kiel.eV., bei der auch online abgestimmt werden kann.
+</p>
+<p>
+	Die online-Wahl geht so vonstatten:
+	<ul>
+		<li>bevor Du Deine Stimme abgeben kannst, musst Du Dich identifizieren
+		<li>dafür musst Du in das folgende Formular Deinen Nachnamen, Dein Geburtsdatum und den Einmal-Code eingeben, den Du mit der Email erhalten hast. Wenn Du diese Seite über den Link in der Email besuchst, ist der Einmal-Code bereits eingetragen.
+	</ul> 
+</p>
+
+<form>
+	
+</form>
+<?php	echo snippet('aa_footer'); ?>
+
