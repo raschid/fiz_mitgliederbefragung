6c9f5f39f951fde2a6c0c0eb28d6bd926ae13cd5
raschid <ralf_schmitt@yahoo.de>
2022-06-15 15:28:50 +0200

    Snippet Questions bearbeitet
    Accordion-Funktion eingebaut.

--- site/snippets/questions/questions.php
+++ site/snippets/questions/questions.php
@@ -1,26 +1,34 @@
 <?php
-
-$questions = $page->questions()->toStructure();
-
-foreach ($questions as $question): ?>
-  <h2><?=
-   $question->qtext()->html(); 
-  ?>
-    </h2><hr>
-<?php endforeach ?>
-
-
-<div class="accordion" id="fiz_umfrage">
+$template = <<<EOD
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
-      </button>
+    <h2 class="accordion-header" id="heading##ITEMNUMBER##">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#carousel##ITEMNUMBER##" aria-expanded="true" aria-controls="collapseOne">##ITEMTITLE##</button>
     </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#fiz_umfrage">
+    <div id="carousel##ITEMNUMBER##" class="accordion-collapse collapse" aria-labelledby="heading##ITEMNUMBER##" data-bs-parent="#fiz_umfrage">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+        ##ITEMBODY##
       </div>
     </div>
   </div>
-</div>
+EOD;
+
+
+
+$questions = $page->questions()->toStructure();
+
+echo '<div class="accordion" id="fiz_umfrage">';
+
+$counter = 1;
+
+foreach ($questions as $question)
+{
+  $t = $template;
+  $itemnumber = $counter;
+  $itemtitle = 'Frage '.$itemnumber.': '.$question->qtext()->html();
+  print str_replace(['##ITEMTITLE##','##ITEMNUMBER##'],[$itemtitle,$itemnumber],$t);
+
+  $counter ++;
+}
+
+echo '</div>';
+?>
