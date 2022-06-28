2838ac3343878057ccbf57d7786f84febe0d41f4
raschid <ralf_schmitt@yahoo.de>
2022-06-28 21:48:14 +0200

    Datenbank auf localhost geändert
    Zugriff auf strato-DB nur über localhost möglich. Deshalb Daten auf Laptop installiert und config.php geändert.
    
    ACHTUNG: beim Import gab es ein Problem mit dem Feld "hatgewaehlt", weil die Datumsangaben alle auf "0000-00-00 00:00:00 " lauteten. Lösung:
    
    SET sql_mode = ''; -> vor der insert-Anweisung!

--- site/config/config.php
+++ site/config/config.php
@@ -6,11 +6,17 @@ return [
   		'install' => true,
         'css' => 'assets/css/custom-panel.css'
   	],
-    'db' => [
+/*    'db' => [
         'host'     => 'rdbms.strato.de',
         'database' => 'dbs7271838',
         'user'     => 'dbu3335239',
         'password' => 'fizmemPOLL16k22'
+    ] */
+    'db' => [
+        'host'     => 'localhost',
+        'database' => 'mitgliederbefragung',
+        'user'     => 'root',
+        'password' => '1root#'
     ]
 ];
 
--- site/templates/home.php
+++ site/templates/home.php
@@ -10,11 +10,17 @@
 	
 
 	else:
+
+
 		echo '<div class="row">';
 		echo snippet('aa_header');
 		echo '<h1 style="margin-bottom:2.0rem">'.$page->heading().'</h1>';
 		echo $page->einleitung()->kirbytext();
 		//echo '</div>';
+
+		$count = Db::count('mitglieder', 'hatgewaehlt = "0000-00-00 00:00:00"');
+		echo 'DB-Count: '.$count;
+
 
 		echo snippet('authenticate_member');
 
