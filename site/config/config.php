467978af999812ba43924b456566a63f7e210fe1
raschid <ralf_schmitt@yahoo.de>
2022-06-28 21:18:36 +0200

    Datenbank zu config.php zugefügt
    ...

--- site/config/config.php
+++ site/config/config.php
@@ -5,6 +5,12 @@ return [
   	'panel' => [
   		'install' => true,
         'css' => 'assets/css/custom-panel.css'
-  	]
+  	],
+    'db' => [
+        'host'     => 'rdbms.strato.de',
+        'database' => 'dbs7271838',
+        'user'     => 'dbu3335239',
+        'password' => 'fizmemPOLL16k22'
+    ]
 ];
 
