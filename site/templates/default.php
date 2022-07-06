82cc1fc5de9b6a12f0b0031bb3996ad3f9efd2be
raschid <ralf_schmitt@yahoo.de>
2022-07-06 08:05:57 +0200

    default template angepasst
    ...

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,13 @@
-
+<?php
+		echo snippet('aa_header');
+		echo '<div class="row">';
+		echo '<div class="col-12">';
+		echo '<h1 class="mt-4" style="margin-bottom:3.0rem">'.$page->title().'</h1>';
+		echo '</div>';
+		echo '</div>';
+		echo '<div class="row" id="main_content">';
+		echo '<div class="col-12 fs-5">';
+		echo $page->text()->kirbytext();
+		echo '</div>';
+		echo snippet('aa_footer');
+?>
